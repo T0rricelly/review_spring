80f86ad86bb0585d21b871cf06a1b152eded94ec
Santiago <kevintorres1609@gmail.com>
2026-03-11 23:30:55 -0500

    mvc

---
+++ SpringMvc/src/main/java/com/mvc/SpringMvc/repository/PersonaRepository.java
@@ -0,0 +1,9 @@
+package com.mvc.SpringMvc.repository;
+
+import com.mvc.SpringMvc.entities.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
+}
---
+++ spring/SpringThymeleaf/src/main/java/com/review/SpringThymeleaf/ControladorInicio.java
@@ -0,0 +1,4 @@
+package com.review.SpringThymeleaf;
+
+public class ControladorInicio {
+}
